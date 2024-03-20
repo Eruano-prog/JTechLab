13bae955723bb826c7ad35313366303067d05915
Eruano <fdgolovlev@gmail.com>
2024-03-21 02:29:33 +0300

    feat: implement CatRepository

--- Lab2/DLL/src/main/java/CatRepository.java
+++ Lab2/DLL/src/main/java/CatRepository.java
@@ -1,30 +1,88 @@
-//import org.hibernate.SessionFactory;
-//
-//public class CatRepository {
-//    void Insert(Cat cat) {
-//        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-//
-//        if (sessionFactory == null) {
-//            return;
-//        }
-//
-//        sessionFactory.inTransaction(session -> {
-//            session.persist(cat);
-//        });
-//    }
-//    public Cat getCatByName(String name) {
-//        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-//
-//        if (sessionFactory == null) {
-//            return null;
-//        }
-//
-////        sessionFactory.inTransaction(session -> {
-////            Cat cat = session.createSelectionQuery("from cats where name = ?1", Cat.class)
-////                    .setParameter(1, name)
-////                    .getSingleResultOrNull();
-////            return cat;
-////        });
-//        return null;
-//    }
-//}
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class CatRepository {
+    void Insert(Cat cat) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Lab2");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try{
+            transaction.begin();
+            entityManager.persist(cat);
+            transaction.commit();
+        }
+        catch (Exception e) {
+            if (transaction.isActive()) transaction.rollback();
+            e.printStackTrace();
+        }
+        finally {
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+    }
+    public Cat getCatByName(String name) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Lab2");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        Cat cat = null;
+        try{
+            transaction.begin();
+            cat = entityManager.find(Cat.class, name);
+            transaction.commit();
+        }
+        catch (Exception e) {
+            if (transaction.isActive()) transaction.rollback();
+            e.printStackTrace();
+        }
+        finally {
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+        return cat;
+    }
+
+    public void update(Cat cat){
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Lab2");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try{
+            transaction.begin();
+            cat = entityManager.merge(cat);
+            transaction.commit();
+        }
+        catch (Exception e) {
+            if (transaction.isActive()) transaction.rollback();
+            e.printStackTrace();
+        }
+        finally {
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+    }
+
+    public void delete(Cat cat){
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Lab2");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try{
+            transaction.begin();
+            entityManager.detach(cat);
+            transaction.commit();
+        }
+        catch (Exception e) {
+            if (transaction.isActive()) transaction.rollback();
+            e.printStackTrace();
+        }
+        finally {
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+    }
+}
